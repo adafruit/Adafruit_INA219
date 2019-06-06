8b6f94f1492408b2a9684414277d1bbcf89b4f9f
Travis CI <travis@travis-ci.org>
2019-06-06 18:37:22 +0000

    Deploy code docs to GitHub Pages Travis build: 22
    Commit: 41e7f6054cd71336facfd6420d2f44ccba06ad78

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['getbusvoltage_5fv',['getBusVoltage_V',['../class_adafruit___i_n_a219.html#a22b4b21b104d6a939b6deb153bfbb8ae',1,'Adafruit_INA219']]],
-  ['getcurrent_5fma',['getCurrent_mA',['../class_adafruit___i_n_a219.html#af28fcc20fb5387ed034e28670d95755c',1,'Adafruit_INA219']]],
-  ['getpower_5fmw',['getPower_mW',['../class_adafruit___i_n_a219.html#a467eb1f41a30b73ceff7145a90102f8b',1,'Adafruit_INA219']]],
-  ['getshuntvoltage_5fmv',['getShuntVoltage_mV',['../class_adafruit___i_n_a219.html#ae4dcd156e9b64583e6fe0d4f48544e87',1,'Adafruit_INA219']]]
+  ['getbusvoltage_5fv',['getBusVoltage_V',['../class_adafruit___i_n_a219.html#ab27c5ff8681cbe04c80bc3efc840dca4',1,'Adafruit_INA219']]],
+  ['getcurrent_5fma',['getCurrent_mA',['../class_adafruit___i_n_a219.html#a962c14fad67d30614fa164e25ebfc158',1,'Adafruit_INA219']]],
+  ['getpower_5fmw',['getPower_mW',['../class_adafruit___i_n_a219.html#a610b73a256146e07c30b00714548f0ac',1,'Adafruit_INA219']]],
+  ['getshuntvoltage_5fmv',['getShuntVoltage_mV',['../class_adafruit___i_n_a219.html#abddaac33a988562fd416cb9949d0c030',1,'Adafruit_INA219']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,9 +1,9 @@
 var indexSectionsWithContent =
 {
-  0: "abgis",
+  0: "abgips",
   1: "a",
   2: "a",
-  3: "abgs",
+  3: "abgps",
   4: "i",
   5: "a"
 };
