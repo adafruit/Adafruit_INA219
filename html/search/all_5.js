e3014e97109afda65ddaf819c96c1c5b4249b59b
Doxygen CI <ci-arduino@invalid>
2021-01-05 16:45:33 +0000

    Deploy docs to GitHub Pages from commit bb528c4d67
    Commit: bb528c4d670180dc87c0f31d3af2427b4e1ca2b2
    GitHub Actions run: 463843016

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['setcalibration_5f16v_5f400ma',['setCalibration_16V_400mA',['../class_adafruit___i_n_a219.html#a3efcbe8cf5ee0a9e8aa6174a1b2390c1',1,'Adafruit_INA219']]],
   ['setcalibration_5f32v_5f1a',['setCalibration_32V_1A',['../class_adafruit___i_n_a219.html#adbff47c27a3b8452c62a2f38672f1cd9',1,'Adafruit_INA219']]],
-  ['setcalibration_5f32v_5f2a',['setCalibration_32V_2A',['../class_adafruit___i_n_a219.html#a1e53272bab1159bab1b0ef255b450d02',1,'Adafruit_INA219']]]
+  ['setcalibration_5f32v_5f2a',['setCalibration_32V_2A',['../class_adafruit___i_n_a219.html#a1e53272bab1159bab1b0ef255b450d02',1,'Adafruit_INA219']]],
+  ['success',['success',['../class_adafruit___i_n_a219.html#a8ce611f54ed202fc667f079d8122beb8',1,'Adafruit_INA219']]]
 ];
